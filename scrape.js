f3026f083e55e0190cceab063c4f1561e20f5c7a
Simon Willison <swillison@gmail.com>
2022-03-13 18:47:16 -0700

    Scrape numComments correctly, refs #1

--- scrape.js
+++ scrape.js
@@ -6,6 +6,10 @@ Array.from(document.querySelectorAll('.itemlist .athing')).map(el => {
   const submitter = el.nextSibling.querySelector('.hnuser').innerText;
   const commentsUrl = el.nextSibling.querySelector('.subtext a:last-child').href;
   const id = commentsUrl.split('?id=')[1];
-  const numComments = parseInt(el.nextSibling.querySelectorAll('.subtext a[href^=item]')[0].innerText.split()[0]);
+  const numComments = parseInt(
+    Array.from(
+      el.nextSibling.querySelectorAll('.subtext a[href^=item]')
+    ).slice(-1)[0].innerText.split()[0]
+  );
   return {id, title, url, dt, points, submitter, commentsUrl, numComments};
 })
