0d7c1e57958dc14c02b43ad709cdb2914287aef2
Simon Willison <swillison@gmail.com>
2022-12-02 13:33:18 -0800

    Script uses zsh
    Bash broke for some reason

---
+++ submit-to-datasette-cloud.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+export SIMONWILLISON_ROWS=$(
+  jq -n --argjson rows "$(cat simonwillison-net.json)" \
+  '{ "rows": $rows, "replace": true }'
+)
+export DATASETTE_ROWS=$(
+  jq -n --argjson rows "$(cat datasette-io.json)" \
+  '{ "rows": $rows, "replace": true }'
+)
+curl -X POST \
+  https://simon.datasette.cloud/data/hacker_news_posts/-/insert \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $DS_TOKEN" \
+  -d $SIMONWILLISON_ROWS
+curl -X POST \
+  https://simon.datasette.cloud/data/hacker_news_posts/-/insert \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $DS_TOKEN" \
+  -d $DATASETTE_ROWS
